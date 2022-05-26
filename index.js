a83ae9e915c01afa1a15c04d73f69dc58cacaeb9
Sean Booker Jr <seanbookerjunior@gmail.com>
2022-05-26 13:14:02 -0400

    commit tree e620c87174ba9ebfdfc129f736d98c07220fe73c

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John",
+    
+    streetAddress: "110 John street",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj}
+
+    newObj[key] = value
+
+    return newObj
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value
+
+    return employee
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+
+    const newObj = {...key}
+
+    delete newObj.key
+
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+
+    delete employee[key]
+
+    return employee
+}
+
